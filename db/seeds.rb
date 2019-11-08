6fab41526dddf4aa11174ae2b6dd1805c5a62be4
volleyok <56710628+volleyok@users.noreply.github.com>
2019-11-08 22:12:59 +0900

    詳細ページ、編集ページ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "create Users"
+User.create!(
+  email: 'user1@example.com',
+  password: 'aaaaaaaa',
+  name: '利樹',
+  self_introduction: '利樹です',
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/1.jpg")
+)
+User.create!(
+  email: 'user2@example.com',
+  password: 'aaaaaaaa',
+  name: '亮',
+  self_introduction: '亮です',
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/2.jpg")
+)
+User.create!(
+  email: 'user3@example.com',
+  password: 'aaaaaaaa',
+  name: '優子',
+  self_introduction: '優子です',
+  sex: 1,
+  img_name: open("#{Rails.root}/db/dummy_img/3.jpg")
+)
+User.create!(
+  email: 'user4@example.com',
+  password: 'aaaaaaaa',
+  name: 'あやみ',
+  self_introduction: 'あやみ',
+  sex: 1,
+  img_name: open("#{Rails.root}/db/dummy_img/4.jpg")
+)
